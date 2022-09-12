bf333ae0a1536e1a830e23563d2b18a8ed3bed25
DmitriyZubelevich <zubelevich.dm@gmail.com>
2022-09-12 11:44:24 +0300

    Update onBeforeInit.js

--- scripts/onBeforeInit.js
+++ scripts/onBeforeInit.js
@@ -6,10 +6,10 @@ for (var i = 0, field; field = jps.settings.main.fields[i]; i++)
 var regions = jelastic.env.control.GetRegions(appid, session);
 if (regions.result != 0) return regions;
 
-if (regions.array.length < 2) {
+if (regions.array.length < 3) {
   fields["bl_count"].markup = "Cluster is not available. " + markup + "Please upgrade your account.";
-  if (regions.array.length < 2)
-    fields["bl_count"].markup = "Package cannot be installed on less than 2 regions. Please contact support or choose a <a href='https://jelastic.cloud/?regions=multiregion' target='_blank'>provider</a> with more regions";  
+  if (regions.array.length < 3)
+    fields["bl_count"].markup = "Package cannot be installed on less than 3 regions. Please contact support or choose a <a href='https://jelastic.cloud/?regions=multiregion' target='_blank'>provider</a> with more regions";  
   fields["bl_count"].cls = "warning";
   fields["bl_count"].hidden = false;
   fields["bl_count"].height = 30;
